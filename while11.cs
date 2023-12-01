c1e196af87266b12a764dde57510606c10a25238
YarikKk1x <152393443+YarikKk1x@users.noreply.github.com>
2023-12-01 16:24:03 +0500

    циклы

---
+++ while11.cs
@@ -0,0 +1,98 @@
+﻿
+ for(int i = 1; i <= 100; i++)
+    Console.WriteLine(i);
+
+/*
+ * for(int i = 2; i <= 200; i = i +2)
+   Console.WriteLine(i);
+*/
+/*
+ * using System.Security.Cryptography.X509Certificates;
+Console.WriteLine("Введите Число");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Число");
+int y = Convert.ToInt32(Console.ReadLine());
+for (int i = x; i <= y; i++)
+    Console.WriteLine(i);
+*/
+
+/*int x = 7;
+for (int i = x; i <= 70; i = i + 7)
+    Console.WriteLine(i);
+*/
+
+
+
+
+
+/*Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int y = Convert.ToInt32(Console.ReadLine());
+int otvet = 1;
+for (int i = 1; i < y; i++)
+    otvet = otvet * x;
+Console.WriteLine(otvet);
+*/
+
+
+/*using System.Diagnostics.CodeAnalysis;
+
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+
+for (int i = x; i <= y; i++)
+{
+    Console.WriteLine(i * i * i);
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*using System.Diagnostics.CodeAnalysis;
+int sum  = 0;
+for (int i = 0; i <= 50; i++)
+{
+    if (i % 5 == 0 || i % 7 == 0)
+     sum += i;
+        
+
+
+
+
+}
+Console.WriteLine(sum);
+*/
+
+/*int x = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (x > 0)
+{
+    x = x / 10;
+    if (x % 2 == 0)
+        sum = sum + x;
+}    
+    Console.WriteLine(sum);
+*/
+
+
+
+/*int x = 1;
+int sum = 0;
+while (x < 100)
+{
+    x = x + 1;
+    sum = sum + x;
+}
+Console.WriteLine(sum);
+*/
